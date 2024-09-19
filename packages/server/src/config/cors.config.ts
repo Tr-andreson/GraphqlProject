bbfa37590ef31ee266c47975d2cc97c2dc0c0f62
tushar <tushar@lovoj.com>
2024-09-19 16:19:36 +0530

    project setup : change cors setting

--- packages/server/src/config/cors.config.ts
+++ packages/server/src/config/cors.config.ts
@@ -3,7 +3,7 @@ import cors from "cors"
 
 export const corsConfig = (app: any) => {
   app.use(cors({
-    origin: process.env.NODE_ENV === "production" ? process.env.FrontEndUrl : ["http://localhost:5173", "https://studio.apollographql.com", "http://localhost:3000"],
+    origin: process.env.NODE_ENV === "production" ? ["http://localhost:5173", "http://localhost:3000"] : ["http://localhost:5173", "https://studio.apollographql.com", "http://localhost:3000"],
     credentials: true
   }))
 
