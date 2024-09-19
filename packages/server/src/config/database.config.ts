67eaa595649346e3daf2dfd3aca1a0d442757a2a
tushar <tushar@lovoj.com>
2024-09-19 16:13:02 +0530

    project setup : server database setup

--- packages/server/src/config/database.config.ts
+++ packages/server/src/config/database.config.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 
 export const databaseConnection = async () => {
-  await mongoose.connect("mongodb://localhost:27017/graph_ecommerce").then(() => {
+  await mongoose.connect(process.env.DATABASE_URL as string).then(() => {
     console.log("Connection Succesfull")
   }).catch((error) => {
     console.log("error" + error)
